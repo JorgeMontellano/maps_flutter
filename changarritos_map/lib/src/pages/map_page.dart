09f472a3a1d17de23b5bca2d52474173fc258627
Jorge Montellano <jorge@etuux.com>
2020-07-06 18:41:53 -0500

    Actualziacion

--- changarritos_map/lib/src/pages/map_page.dart
+++ changarritos_map/lib/src/pages/map_page.dart
@@ -20,7 +20,7 @@ class _MapPageState extends State<MapPage> {
         body: new FlutterMap(
           options: MapOptions(
             center: LatLng(22.574980, -102.247576),
-            zoom: 12.0,
+            zoom: 15.0,
           ),
           layers: [
             TileLayerOptions(
@@ -29,16 +29,19 @@ class _MapPageState extends State<MapPage> {
                 additionalOptions: {
                   'accessToken':
                       'pk.eyJ1Ijoiam1vbnRlbGxhbm8iLCJhIjoiY2tjYXc4am41MXpndTJ4bzZlanRpNzBhciJ9.J44_QjsQboYA8EobUc4sVg',
-                  'id': 'mapbox.streets',
+                  'id': 'mapbox.streets'
                 }),
             MarkerLayerOptions(
               markers: [
                 Marker(
-                    width: 80.0,
-                    height: 80.0,
+                    width: 120.0,
+                    height: 120.0,
                     point: LatLng(22.575683, -102.247447),
-                    builder: (ctx) =>
-                        Container(child: Icon(Icons.add_location))),
+                    builder: (ctx) => Container(
+                            child: Icon(
+                          Icons.location_on,
+                          size: 50.0,
+                        ))),
               ],
             ),
           ],
